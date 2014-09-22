cb3b3798e24e7a320d3a7f12b5ca1425549b9d06
zlakomanoff <zlakomanoff@gmail.com>
2014-09-22 17:55:23 +0400

    nohup for forked server listener

--- distributor.sh
+++ distributor.sh
@@ -4,10 +4,10 @@ read message
 
 if [[ "$message" = 'give_me_port' ]]
 then
-	count=`ls *.fifo | wc -l`
-	port=$(($count+6201))
-	#nc -l -p $port -e client_listener.sh &
-	echo $port
+	count=$(ls *.fifo | wc -l)
+	port=$(($count+5001))
+	nohup nc -l -p $port -e client_listener.sh &
+	echo $port 
 else
 	echo 'wrong_message'
 fi
