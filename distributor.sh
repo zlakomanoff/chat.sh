24a9063a20c4a792bf8647ee8aca85819fc95455
zlakomanoff <zlakomanoff@gmail.com>
2014-09-23 23:33:10 +0400

    change select port logic

--- distributor.sh
+++ distributor.sh
@@ -2,12 +2,17 @@
 
 read message
 
-if [[ "$message" = 'give_me_port' ]]
+if [[ "$message" == 'give_me_port' ]]
 then
-	count=$(ls *.fifo | wc -l)
-	port=$(($count+50000))
-	nohup nc -l -p $port -e client_listener.sh <&- >&- &
-	echo $port 
+	while true; do
+		port=50$(($RANDOM%10))$(($RANDOM%10))$(($RANDOM%10))
+		usage=$(lsof -i :$port | wc -l)
+		if [ $usage -eq 0 ]; then
+			break
+		fi
+	done
+	coproc $count { nc -l -p $port -e client_listener.sh; }
+	echo $port
 else
 	echo 'wrong_message'
 fi
